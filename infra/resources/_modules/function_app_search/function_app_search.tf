ffcd8beb2c9a43fa8f4d56cd13bfe315040ebaf5
Michael Disaro <michael.disaro@gmail.com>
2024-09-13 12:38:21 +0200

    fixed hc path

--- infra/resources/_modules/function_app_search/function_app_search.tf
+++ infra/resources/_modules/function_app_search/function_app_search.tf
@@ -11,7 +11,7 @@ module "function_app_cgn_search" {
   }
 
   resource_group_name = var.resource_group_name
-  health_check_path   = "/api/v1/info"
+  health_check_path   = "/api/v1/cgn/operator-search/info"
   node_version        = 20
 
   subnet_cidr                          = var.cidr_subnet_cgn_search_func
