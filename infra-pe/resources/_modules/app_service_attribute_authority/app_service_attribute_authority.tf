7fb2efae60ce3e33c0b1e99064d1896e0f0a0e0c
Michael Disarò <michael.disaro@pagopa.it>
2024-11-27 10:54:52 +0100

    added comments

--- infra-pe/resources/_modules/app_service_attribute_authority/app_service_attribute_authority.tf
+++ infra-pe/resources/_modules/app_service_attribute_authority/app_service_attribute_authority.tf
@@ -15,10 +15,12 @@ module "app_service_attribute_authority" {
   node_version        = 20
 
   tier = var.attribute_authority_tier
-
+  
+  # SETTINGS
   app_settings      = local.attribute_authority.app_settings
   slot_app_settings = local.attribute_authority.app_settings
 
+  # SUBNET
   subnet_cidr                          = var.cidr_subnet_cgn_attribute_authority
   subnet_pep_id                        = var.private_endpoint_subnet_id
   private_dns_zone_resource_group_name = var.private_dns_zone_resource_group_name
