9de4b56a15395bdb28868fcbda76ab04f60a367c
Michael Disarò <michael.disaro@pagopa.it>
2024-12-02 12:21:48 +0100

    removed pg bouncer

--- infra-pe/resources/prod/postgresql_db.tf
+++ infra-pe/resources/prod/postgresql_db.tf
@@ -28,7 +28,7 @@ module "postgresql_db" {
   db_version            = 16
   storage_mb            = 262144
   backup_retention_days = 14
-  pgbouncer_enabled     = true
+  pgbouncer_enabled     = false
 
   tags = local.tags
 }
