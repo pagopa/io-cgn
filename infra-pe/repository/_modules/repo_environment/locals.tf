ec0900b2d62c7fe985761715843c92425caef273
christian-calabrese <christian.calabrese@pagopa.it>
2024-12-03 11:33:15 +0100

    feat: add vpn

--- infra-pe/repository/_modules/repo_environment/locals.tf
+++ infra-pe/repository/_modules/repo_environment/locals.tf
@@ -10,7 +10,7 @@ locals {
     secrets = {
       "ARM_CLIENT_ID"       = data.azurerm_user_assigned_identity.identity_ci.client_id
       "ARM_SUBSCRIPTION_ID" = data.azurerm_subscription.current.subscription_id
-      "ARM_TENANT_ID" = data.azurerm_client_config.current.tenant_id
+      "ARM_TENANT_ID"       = data.azurerm_client_config.current.tenant_id
     }
   }
 
@@ -18,7 +18,7 @@ locals {
     secrets = {
       "ARM_CLIENT_ID"       = data.azurerm_user_assigned_identity.identity_cd.client_id
       "ARM_SUBSCRIPTION_ID" = data.azurerm_subscription.current.subscription_id
-      "ARM_TENANT_ID" = data.azurerm_client_config.current.tenant_id
+      "ARM_TENANT_ID"       = data.azurerm_client_config.current.tenant_id
     }
 
     reviewers_teams = ["io-cgn-contributors", "engineering-team-cloud-eng"]
@@ -28,7 +28,7 @@ locals {
     secrets = {
       "ARM_CLIENT_ID"       = data.azurerm_user_assigned_identity.identity_app_cd.client_id
       "ARM_SUBSCRIPTION_ID" = data.azurerm_subscription.current.subscription_id
-      "ARM_TENANT_ID" = data.azurerm_client_config.current.tenant_id
+      "ARM_TENANT_ID"       = data.azurerm_client_config.current.tenant_id
     }
 
     reviewers_teams = ["io-cgn-contributors", "engineering-team-cloud-eng"]
