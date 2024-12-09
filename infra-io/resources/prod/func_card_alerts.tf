cc75add1a396f6b1f66427756fbfcf2a30f7473b
Michael Disarò <michael.disaro@pagopa.it>
2024-12-09 16:00:03 +0100

    added alerts

--- infra-io/resources/prod/func_card_alerts.tf
+++ infra-io/resources/prod/func_card_alerts.tf
@@ -289,3 +289,37 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "expired_eyca_failure_
 
   tags = local.tags
 }
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "messages_failure_alert_rule" {
+  enabled             = true
+  name                = "[CGN | iopstcgn] Failures on messages-poison"
+  resource_group_name = azurerm_resource_group.itn_cgn.name
+  location            = local.location
+
+  scopes                  = [data.azurerm_storage_account.storage_cgn.id]
+  description             = "Permanent failures processing Messages. REQUIRED MANUAL ACTION"
+  severity                = 1
+  auto_mitigation_enabled = false
+
+  window_duration      = "PT15M" # Select the interval that's used to group the data points by using the aggregation type function. Choose an Aggregation granularity (period) that's greater than the Frequency of evaluation to reduce the likelihood of missing the first evaluation period of an added time series.
+  evaluation_frequency = "PT15M" # Select how often the alert rule is to be run. Select a frequency that's smaller than the aggregation granularity to generate a sliding window for the evaluation.
+
+  criteria {
+    query                   = <<-QUERY
+      StorageQueueLogs
+        | where OperationName has "PutMessage"
+        | where Uri has "messages-poison"
+      QUERY
+    operator                = "GreaterThan"
+    threshold               = 0
+    time_aggregation_method = "Count"
+  }
+
+  action {
+    action_groups = [
+      azurerm_monitor_action_group.io_p_itn_cgn_error_action_group.id,
+    ]
+  }
+
+  tags = local.tags
+}
