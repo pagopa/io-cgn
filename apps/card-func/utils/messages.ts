caa14c746eda9a186d91ef9b6f43195030730ec4
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2024-12-23 16:01:05 +0100

    [#PE-828] Fixed card messages (#35)

--- apps/card-func/utils/messages.ts
+++ apps/card-func/utils/messages.ts
@@ -13,69 +13,33 @@ export enum MessageTypeEnum {
 }
 
 export const MESSAGES = {
-  CardRevoked: (card: CardRevoked): MessageContent =>
-    ({
+  CardRevoked: (card: CardRevoked): MessageContent => {
+    const formattedRevocationDate = format(card.revocation_date, "dd-MM-yyyy");
+    return {
       subject: "La tua carta è stata revocata",
-      markdown: `
-Ti avvisiamo che la tua Carta Giovani Nazionale è stata revocata il giorno ${format(
-        card.revocation_date,
-        "dd-MM-yyyy"
-      )} per ${card.revocation_reason}.
-            
-Non sarà più possibile utilizzare la carta né nei punti fisici né online.`
-    } as MessageContent),
+      markdown: `Ti avvisiamo che la tua Carta Giovani Nazionale è stata revocata il giorno ${formattedRevocationDate} per ${card.revocation_reason}.\n\nNon sarà più possibile utilizzare la carta né nei punti fisici né online.`
+    } as MessageContent;
+  },
   CardActivated: (): MessageContent =>
     ({
       subject: "La tua Carta Giovani è attiva!",
-      markdown: `---
-it:
-    cta_1: 
-        text: "Usa la Carta"
-        action: "ioit://CGN_DETAILS"
-en:
-    cta_1: 
-        text: "Use Card"
-        action: "ioit://CGN_DETAILS"
----
-Buone notizie! La tua Carta Giovani Nazionale è attiva. Vai alla sezione Portafoglio per visualizzarla e iniziare a usarla.
-
-Ricorda che solo tu puoi accedere alle opportunità messe a disposizione dai partner.
-
-La Carta Giovani Nazionale sarà valida da oggi fino al compimento dei 36 anni.
-
-Inizia subito a usarla!
+      markdown: `---\nit:\n    cta_1: \n        text: "Usa la Carta"\n        action: "ioit://CGN_DETAILS"\nen:\n    cta_1: \n        text: "Use Card"\n        action: "ioit://CGN_DETAILS"\n---\nBuone notizie! La tua Carta Giovani Nazionale è attiva. Vai alla sezione Portafoglio per visualizzarla e iniziare a usarla.\n\nRicorda che solo tu puoi accedere alle opportunità messe a disposizione dai partner.\n\nLa Carta Giovani Nazionale sarà valida da oggi fino al compimento dei 36 anni.\n\nInizia subito a usarla!
 `
     } as MessageContent),
   CardExpired: (): MessageContent =>
     ({
       subject: "La tua Carta Giovani Nazionale è scaduta",
-      markdown: `Ti avvisiamo che da oggi non è più possibile utilizzare la tua Carta Giovani Nazionale.
-
-Grazie per aver partecipato all'iniziativa!`
+      markdown: `Ti avvisiamo che da oggi non è più possibile utilizzare la tua Carta Giovani Nazionale.\n\nGrazie per aver partecipato all'iniziativa!`
     } as MessageContent),
   EycaExpired: (): MessageContent =>
     ({
       subject: "La tua Carta EYCA è scaduta",
-      markdown: `Ti avvisiamo che da oggi non è più possibile utilizzare la tua Carta Giovani Nazionale per acquisti sul circuito EYCA.
-    
-    La Carta rimane valida per gli acquisti in Italia!`
+      markdown: `Ti avvisiamo che da oggi non è più possibile utilizzare la tua Carta Giovani Nazionale per acquisti sul circuito EYCA.\n\nLa Carta rimane valida per gli acquisti in Italia!`
     } as MessageContent),
   CardError: (): MessageContent =>
     ({
       subject: "Abbiamo riscontrato dei problemi",
-      markdown: `---
-      it:
-          cta_1: 
-              text: "Riprova"
-              action: "ioit://CTA_START_CGN"
-      en:
-          cta_1: 
-              text: "Retry"
-              action: "ioit://CTA_START_CGN"
-      ---
-      Ci dispiace, ma per un errore nella lavorazione della tua richiesta, non è stato possibile attivare Carta Giovani Nazionale.
-      
-      Prova ad inserire una nuova richiesta.
+      markdown: `---\nit:\n    cta_1: \n        text: "Riprova"\n        action: "ioit://CTA_START_CGN"\nen:\n        cta_1: \n        text: "Retry"\n        action: "ioit://CTA_START_CGN"\n---\nCi dispiace, ma per un errore nella lavorazione della tua richiesta, non è stato possibile attivare Carta Giovani Nazionale.\n\nProva ad inserire una nuova richiesta.
       `
     } as MessageContent)
 };
