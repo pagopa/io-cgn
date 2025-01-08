8e34a58d55cfb9a52e4e4e595444c2eb157cdaff
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-08 11:17:47 +0100

    Fix gh runner environments (#37)

--- infra-io/github-runner/prod/main.tf
+++ infra-io/github-runner/prod/main.tf
@@ -22,14 +22,24 @@ provider "azurerm" {
 module "container_app_job_selfhosted_runner" {
   source = "github.com/pagopa/dx//infra/modules/github_selfhosted_runner_on_container_app_jobs?ref=main"
 
-  prefix    = local.prefix
-  env_short = local.env_short
+  resource_group_name = "${local.prefix}-${local.env_short}-itn-cgn-rg-01"
 
-  repo_name = local.repo_name
+  environment = {
+    prefix    = local.prefix
+    env_short = local.env_short
+    location  = "italynorth"
+    instance_number = "01"
+  }
+
+  repository = {
+    name = local.repo_name
+  }
 
   container_app_environment = {
-    name                = "${local.prefix}-${local.env_short}-github-runner-cae"
-    resource_group_name = "${local.prefix}-${local.env_short}-github-runner-rg"
+    id                  = data.azurerm_container_app_environment.cae.id
+    location            = data.azurerm_container_app_environment.cae.location
+    use_labels          = true
+    override_labels     = ["${local.prefix}-${local.env}"]
   }
 
   key_vault = {
@@ -39,4 +49,3 @@ module "container_app_job_selfhosted_runner" {
 
   tags = local.tags
 }
-
