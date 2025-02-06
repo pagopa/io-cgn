3b6575e333c37183ca67cf86dc0135c8f2c9e8c8
Michael Disarò <michael.disaro@pagopa.it>
2025-02-06 12:34:49 +0100

    fixed pep name

--- infra-io/resources/_modules/function_app_search/networking.tf
+++ infra-io/resources/_modules/function_app_search/networking.tf
@@ -1,11 +1,11 @@
 resource "azurerm_private_endpoint" "postgresql_db_pe_itn" {
-  name                = "${var.project}-${var.domain}-psql-pep-01"
+  name                = "${var.project}-${var.domain}-psql-pep-${var.instance_number}"
   location            = var.location
   resource_group_name = var.resource_group_name
   subnet_id           = var.private_endpoint_subnet_id
 
   private_service_connection {
-    name                           = "${var.project}-${var.domain}-psql-pep-01"
+    name                           = "${var.project}-${var.domain}-psql-pep-${var.instance_number}"
     private_connection_resource_id = "/subscriptions/74da48a3-b0e7-489d-8172-da79801086ed/resourceGroups/io-p-itn-cgn-pe-rg-01/providers/Microsoft.DBforPostgreSQL/flexibleServers/io-p-itn-cgn-pe-postgresql-psql-01"
     is_manual_connection           = false
     subresource_names              = ["postgresqlServer"]
