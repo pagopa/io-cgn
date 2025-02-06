9993ea2a1e11c43c141642dc344c3919ff034ea2
Michael Disarò <michael.disaro@pagopa.it>
2025-02-06 15:15:06 +0100

    moved pep outside function module

---
+++ infra-io/resources/prod/psql_pe_private_endpoint.tf
@@ -0,0 +1,12 @@
+module "psql_pe_private_endpoint" {
+  source = "../_modules/psql_pe_private_endpoint"
+
+  location = local.location
+  project  = local.project
+  domain   = local.domain
+
+  resource_group_name        = azurerm_resource_group.itn_cgn.name
+  private_endpoint_subnet_id = data.azurerm_subnet.pep.id
+
+  tags = local.tags
+}
