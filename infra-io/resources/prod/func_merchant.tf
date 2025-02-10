a49d09432d16276ccc57022110d897050906e012
Michael Disaro <15191694+michaeldisaro@users.noreply.github.com>
2025-02-10 14:11:54 +0100

    [#PE-944] Removed cgn-xxx-01 functions (#55)

--- infra-io/resources/prod/func_merchant.tf
+++ infra-io/resources/prod/func_merchant.tf
@@ -1,34 +1,3 @@
-module "functions_cgn_merchant" {
-  source = "../_modules/function_app_merchant"
-
-  prefix          = local.prefix
-  env_short       = local.env_short
-  location        = local.location
-  project         = local.project
-  domain          = local.domain
-  instance_number = "01"
-
-  resource_group_name = azurerm_resource_group.itn_cgn.name
-
-  ai_instrumentation_key = data.azurerm_application_insights.common.instrumentation_key
-  ai_connection_string   = data.azurerm_application_insights.common.connection_string
-  ai_sampling_percentage = 5
-
-  cidr_subnet_cgn_merchant_func        = "10.20.12.0/26"
-  private_endpoint_subnet_id           = data.azurerm_subnet.pep.id
-  private_dns_zone_resource_group_name = data.azurerm_resource_group.weu_common.name
-  virtual_network = {
-    resource_group_name = data.azurerm_virtual_network.vnet_common_itn.resource_group_name
-    name                = data.azurerm_virtual_network.vnet_common_itn.name
-  }
-
-  redis_url      = module.redis_cgn.hostname
-  redis_port     = module.redis_cgn.ssl_port
-  redis_password = module.redis_cgn.primary_access_key
-
-  tags = local.tags
-}
-
 module "functions_cgn_merchant_02" {
   source = "../_modules/function_app_merchant"
 
