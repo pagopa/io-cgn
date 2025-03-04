179d1ca2367130851c3f288a6f7f199fe6b474e2
Michael Disarò <michael.disaro@pagopa.it>
2025-03-04 14:40:28 +0100

    fix: linter

--- apps/search-func/models/MerchantProfileModel.ts
+++ apps/search-func/models/MerchantProfileModel.ts
@@ -7,9 +7,9 @@ export default class MerchantProfileModel extends Model {
   public readonly all_national_addresses!: boolean;
   public readonly description!: string;
   public readonly discount_code_type!: DiscountCodeTypeEnumModel | undefined;
+  public readonly full_name!: string;
   public readonly image_url!: string | undefined;
   public readonly name!: string;
-  public readonly full_name!: string;
   public readonly profile_k!: number;
   public readonly website_url!: string | undefined;
 }
