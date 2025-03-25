23cf5f8b248f668999ade8363d64ee9d1a10a80d
Krusty93 <andrea.grillo@pagopa.it>
2025-03-25 10:49:00 +0100

    remove import block

--- infra-io/identity/prod/resource_groups.tf
+++ infra-io/identity/prod/resource_groups.tf
@@ -1,8 +1,3 @@
-import {
-  id = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-itn-cgn-rg-01"
-  to = azurerm_resource_group.cgn_itn_01
-}
-
 resource "azurerm_resource_group" "cgn_itn_01" {
   name     = "${local.project}-itn-${local.domain}-rg-01"
   location = local.location
