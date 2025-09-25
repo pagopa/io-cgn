46fda3347100e6b2568727db03e6fc3bda52a89a
michaeldisaro <michael.disaro@gmail.com>
2025-09-25 10:18:55 +0200

    init infra agw

---
+++ infra-pe/resources/_modules/application_gateway/variables.tf
@@ -0,0 +1,72 @@
+variable "prefix" {
+  type        = string
+  description = "IO Prefix"
+}
+
+variable "env_short" {
+  type        = string
+  description = "Short environment"
+}
+
+variable "project" {
+  type        = string
+  description = "IO prefix, short environment and short location"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
+variable "domain" {
+  type        = string
+  description = "Domain"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Resource tags"
+}
+
+variable "azure_subscription_id" {
+  type        = string
+  description = "Subscription id"
+  sensitive   = true
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group where resources will be created"
+}
+
+variable "sku" {
+  type = object({
+    name = string
+    tier = string
+  })
+  description = "Sku settings"
+}
+
+
+variable "virtual_network" {
+  type = object({
+    name                = string
+    resource_group_name = string
+  })
+  description = "Virtual network to create subnet in"
+}
+
+variable "cidr_subnet_cgn_pe_appgateway" {
+  type        = string
+  description = "CIDR block for AGW subnet"
+}
+
+variable "key_vault_name" {
+  type        = string
+  description = "Key Vault name"
+}
+
+variable "apim_hostname" {
+  type        = string
+  description = "APIM hostname"
+}
