c311d87627afed4714f34f9ec2b437e61d5b6eb9
michaeldisaro <michael.disaro@gmail.com>
2026-05-19 15:56:46 +0200

    fixed permissions identity

--- infra-pe/identity/prod/main.tf
+++ infra-pe/identity/prod/main.tf
@@ -19,6 +19,12 @@ provider "azurerm" {
   }
 }
 
+provider "azurerm" {
+  features {}
+  alias           = "io"
+  subscription_id = "ec285037-c673-4f58-b594-d7c480da4e8b"
+}
+
 module "federated_identities" {
   source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
 
@@ -57,4 +63,35 @@ module "app_federated_identities" {
     enable = true,
     roles  = local.environment_app_cd_roles
   }
+}
+
+# CI needs Reader to initialize the azurerm.io provider and read data sources
+resource "azurerm_role_assignment" "ci_reader_io_subscription" {
+  provider             = azurerm.io
+  scope                = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b"
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_user_assigned_identity.ci.principal_id
+}
+
+# CD needs Reader + RBAC admin to create role assignments on the CosmosDB
+resource "azurerm_role_assignment" "cd_reader_io_subscription" {
+  provider             = azurerm.io
+  scope                = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b"
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_user_assigned_identity.cd.principal_id
+}
+
+resource "azurerm_role_assignment" "cd_rbac_admin_io_rg_cgn" {
+  provider             = azurerm.io
+  scope                = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-cgn"
+  role_definition_name = "Role Based Access Control Administrator"
+  principal_id         = data.azurerm_user_assigned_identity.cd.principal_id
+}
+
+# For azurerm_cosmosdb_sql_role_assignment, the CD identity also needs DocumentDB Account Contributor
+resource "azurerm_role_assignment" "cd_cosmosdb_contributor_io_rg_cgn" {
+  provider             = azurerm.io
+  scope                = "/subscriptions/ec285037-c673-4f58-b594-d7c480da4e8b/resourceGroups/io-p-rg-cgn"
+  role_definition_name = "DocumentDB Account Contributor"
+  principal_id         = data.azurerm_user_assigned_identity.cd.principal_id
 }
