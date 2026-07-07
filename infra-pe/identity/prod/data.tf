3bb39777820cedb72a45d7edc4d1f2310272ccc2
Andrea Grillo <andrea.grillo@pagopa.it>
2026-07-07 09:11:32 +0200

    Remove generated Terraform docs from infrastructure (#150)

--- infra-pe/identity/prod/data.tf
+++ infra-pe/identity/prod/data.tf
@@ -1,11 +1,11 @@
 data "azurerm_user_assigned_identity" "ci" {
-  provider            = azurerm   # PE subscription
+  provider            = azurerm # PE subscription
   name                = module.federated_identities.federated_ci_identity.name
   resource_group_name = module.federated_identities.federated_ci_identity.resource_group_name
 }
 
 data "azurerm_user_assigned_identity" "cd" {
-  provider            = azurerm   # PE subscription
+  provider            = azurerm # PE subscription
   name                = module.federated_identities.federated_cd_identity.name
   resource_group_name = module.federated_identities.federated_cd_identity.resource_group_name
 }
